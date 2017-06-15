9bb160f04c46b84007c72cebcf20f96779890b0c
radhikap18 <radhikaparalkar814@gmail.com>
2017-06-15 08:04:21 -0400

    Create hourglass.cpp

--- hourglass/hourglass.cpp
+++ hourglass/hourglass.cpp
@@ -1,13 +1,16 @@
 /*
+ * Digital Hourglass
  * Author: Radhika Paralkar
  * Copyright (c) 2016 Linaro Ltd.
  * All rights reserved.
+ ********************************
+ * The LCD is at I2C0
  *
  */
 
 #include <iostream>
 #include <string>
-#include "../../include/upm/jhd1313m1.hpp"
+#include "upm/jhd1313m1.hpp"
 
 #define I2C_BUS  0
 
@@ -16,41 +19,48 @@ upm::Jhd1313m1* lcd;
 
 int main()
 {
-	int interval, timer_time;
-	lcd = new upm::Jhd1313m1(I2C_BUS, 0x3e, 0x62);
+    int interval, timer_time;
+    lcd = new upm::Jhd1313m1(I2C_BUS, 0x3e, 0x62);
 
-
-	lcd->clear();
-	lcd->setColor(0x00,0x00,0xff); //blue
-	lcd->setCursor(0,0); 
-	lcd->write("Enter time in");
+    /*Clear the LCD screen and print initial message*/
+    lcd->clear();
+    lcd->setColor(0x00,0x00,0xff); //Blue
+    lcd->setCursor(0,0); 
+    lcd->write("Enter time in");
     lcd->setCursor(1,0);
     lcd->write("minutes...");
 
+    /* For the time to be entered via basic input */
     cout << "Enter the time in minutes:" << endl;
     cin >> timer_time;
 
+    /* Convert time to seconds and create equal intervals for sleep */
     interval = (timer_time*60)/30;
 
-	lcd->clear();
-    lcd->setColor(0xff, 0xff, 0xff);
+    /* Clear initial message */
+    lcd->clear();
+    lcd->setColor(0xff, 0xff, 0xff); //White
 
+    /* The two loops move the cursor position after each interval
+    and print a character at that position */
     for(int i=0;i<2;i++)
     {
         for(int j=0;j<15;j++)
         {
                 lcd->setCursor(i,j);
-                lcd->write("*");
+                lcd->write("*"); 
                 sleep(interval);
         }
     }
 
+    /* Clear screen to print next message */
     lcd->clear();
     lcd->write("TIME UP!!");
     sleep(10);
 
-	delete lcd;
-	return 0;
+    /* Delete allocated memory to avoid memory leaks */
+    delete lcd;
+    return 0;
 }
 
 
